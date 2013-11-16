db9108b66b1d988e49d66641e00b54e733623554
Anthony Erlinger <aerlinger@gmail.com>
2013-11-16 15:33:17 -0500

    Algorithm for calculating corr. coeff. between two lines

--- lib/chorus_extractor/line_matcher.rb
+++ lib/chorus_extractor/line_matcher.rb
@@ -1,19 +1,50 @@
 module ChorusExtractor
-    class << self
+  class << self
+    # Counts the number of ordered similarities between line1 and line2
+    def num_similarities(line1, line2)
+      Diff::LCS::Internals.lcs(line1, line2).compact.length
+    end
 
-      # A simple wrapper for Diff::LCS.diff that counts the
-      def num_differences(line1, line2)
-        Diff::LCS.diff(line1, line2).count
-      end
+    # Counts the number of changes necessary to convert line1 into line2
+    def num_deltas(line1, line2)
+      Array(Diff::LCS.diff(line1, line2))[0].length
+    end
 
-      def correlation_coeff(line1, line2)
-        max_line_length = [line1.length, line2.length].max
+    # Returns a two element array of line1 and line2 sorted by string length
+    def shortest_longest(line1, line2)
+      [line1, line2].sort { |n| n.length }
+    end
 
-        (max_line_length - num_differences(line1, line2)).to_f / max_line_length
-      end
+    # Returns a float between 0-1 indicating number of changes required to turn line1 into line2
+    # divided by the total possible changes required to change line1 into line2
+    def delta_coefficient(line1, line2)
+      shorter_line, longer_line = shortest_longest(line1, line2)
+      return 0 if longer_line.length.zero?
 
-      def compute_correlation_matrix(text)
+      changes_count = num_deltas(shorter_line, longer_line)
+      total_possible_changes_count = shorter_line.length + longer_line.length
 
+      (total_possible_changes_count - changes_count.to_f)/total_possible_changes_count
+    end
+
+    # Returns a coefficient (float) indicating the number of ordered similarities between the two strings
+    # divided by the length of the longer string
+    def overlap_coefficient(line1, line2)
+      shorter_line, longer_line = shortest_longest(line1, line2)
+      return 0 if longer_line.length.zero?
+
+      similar_letters_count = num_similarities(shorter_line, longer_line)
+
+      similar_letters_count.to_f/longer_line.length
+    end
+
+    def correlation_coeff(line1, line2)
+      if ENV['MATCH_ALGORITHM'] = 'overlap'
+        overlap_coefficient(line1, line2)
+      else
+        #overlap_coefficient(line1, line2)
+        delta_coefficient(line1, line2)
       end
     end
+  end
 end
